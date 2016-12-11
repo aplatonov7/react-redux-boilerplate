a2523dc315785290e41e31e0cbf01a639edc4958
Artem Platonov <discodesu@gmail.com>
2016-12-11 21:40:17 +0400

    Refactoring server code, updating ExtractTextPlugin, merging webpack configs

--- bin/build.js
+++ bin/build.js
@@ -1,10 +1,10 @@
-/* Build script for production config */
+/* global rm mkdir cp */
 
 import 'shelljs/global'
 
 import webpack from 'webpack'
 import path from 'path'
-import conf from '../webpack.config.prod'
+import conf from '../webpack.config'
 
 const distPath = path.join(__dirname, '../dist')
 const distStaticPath = path.join(distPath, 'static')
@@ -15,17 +15,17 @@ rm('-rf', distPath)
 mkdir(distPath)
 
 /* Building our project via webpack using prod config */
-webpack(conf, (err, stats) => {
+webpack(conf({ prod: true }), (err, stats) => {
   if (err) throw err
 
   /* Copying static folder from /app to /dist after successful build */
   cp('-R', appStaticPath, distStaticPath)
 
-  process.stdout.write(stats.toString({
+  process.stdout.write(`${stats.toString({
     colors: true,
     modules: false,
     children: false,
     chunks: false,
     chunkModules: false,
-  }) + '\n')
-});
+  })}\n`)
+})
